96760885356816c1d506b60637233a9b3e7ec90d
Matuzenko Olexandr <omatuzenko@gmail.com>
2018-12-29 20:39:51 -0200

    added cards list view on activity widget

---
+++ lib/model/activity.dart
@@ -0,0 +1,10 @@
+class Activity {
+  String title;
+  String level;
+  double indicatorValue;
+  int price;
+  String content;
+
+  Activity(
+      {this.title, this.level, this.indicatorValue, this.price, this.content});
+}
