e91913a28a4fd1e8a086067393e6ef837197a642
Alex Matuzenko <omatuzenko@gmail.com>
2019-06-09 15:53:01 +0300

    Added view pager parallax app

---
+++ lib/appviewpagerparallax/main_viewpager_parallax.dart
@@ -0,0 +1,16 @@
+import 'home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'SF Pro Display'),
+      title: 'Buy Tickets',
+      home: HomePage(),
+    );
+  }
+}
